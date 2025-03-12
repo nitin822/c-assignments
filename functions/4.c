4123a02db47325f5e6d699dfe825183e53a75e58
nitin822 <nkp10082005@gmail.com>
2025-03-12 07:27:06 +0530

    c

---
+++ functions/4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int square(int num) {
+    return num * num;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Square of %d is %d.\n", num, square(num));
+
+    return 0;
+}
